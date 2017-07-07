a1c3a245be6f97e1c99ba43603960b11a3c02de6
lasse <L.Schultebraucks@t-online.de>
2017-07-07 09:50:47 +0200

    fixed last commit

---
+++ src/app/blog/blogpost-list/blog-post/blogpost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blogpost',
+  templateUrl: './blog-post.component.html',
+  styleUrls: ['./blog-post.component.css']
+})
+export class BlogPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/blog/blogpost-list/blogpost-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blogpost-list',
+  templateUrl: './blogpost-list.component.html',
+  styleUrls: ['./blogpost-list.component.css']
+})
+export class BlogpostListComponent implements OnInit {
+  blogposts = [
+    {
+      title: "Lorem ipsum",
+      url: "lorem-ipsum",
+      date: "17 June 2017",
+      previewText: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+      image: "http://wallpapercave.com/wp/n0FcaBH.jpg"
+    },
+    {
+      title: "Ipsum Lorem",
+      url: "ipsum-lorem",
+      date: "16 June 2017",
+      previewText: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+      image: "https://image.jimcdn.com/app/cms/image/transf/dimension=372x1024:format=jpg/path/s18122fe3bc485736/image/i648b858271624317/version/1495393615/image.jpg"
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

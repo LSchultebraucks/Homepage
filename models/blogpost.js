9bae74f2104a217955046b517c25aa6403cda2c2
lasse <L.Schultebraucks@t-online.de>
2017-07-09 18:34:21 +0200

    update blogpost model

--- models/blogpost.js
+++ models/blogpost.js
@@ -5,11 +5,11 @@ let schema = new Schema({
   title: { type: String, required: true },
   url: { type: String, required: true },
   date:  { type: Date, default: Date.now },
-  image: { type: Schema.Types.ObjectId, ref: "fs.files", required: true }, // GRIDFS
+  image: { type: String, required: true }, // GRIDFS
   tags:  [{type: String, required: true}],
   intro: { type: String, required: true },
   // SOME HTML TEMPLATE WITH PICS ALL SAVED IN GRIDFS
-  template: { type: Schema.Types.ObjectId, ref: "fs.files", required: true }
+  template: { type: String, required: true }
 });
 
 schema.plugin(mongooseUniqueValidator);

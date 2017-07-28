d812707efa17f0fa51a6dd95e07b574c9f91d418
lasse <L.Schultebraucks@t-online.de>
2017-07-28 08:29:21 +0200

    added mlab

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   dev: {
     port: process.env.port || 3000,
-    db  : process.env.DB_LINK || "mongodb://127.0.0.1/homepage"
+    db  : process.env.DB_LINK || 'mongodb://lschultebraucks:thelord234@ds127153.mlab.com:27153/homepage'
   },
   prod: {
     // TODO
